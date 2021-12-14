6a9027fea8a7dbaf2e4fa651df65e308b44d8659
Arif-Rahman-Sikder <92696767+Arif-Rahman-Sikder@users.noreply.github.com>
2021-12-14 14:27:03 +0600

    Delete Say Hello With C++.cpp

--- Say Hello With C++.cpp
+++
@@ -1,9 +0,0 @@
- #include<iostream>
- using namespace std;
- int main(){
-     string S;
-     cin>>S; //cin is used to take user input
-     cout<<"Hello, "<<S<<endl; //cout is used for output objective
-
-     return 0;
- }
