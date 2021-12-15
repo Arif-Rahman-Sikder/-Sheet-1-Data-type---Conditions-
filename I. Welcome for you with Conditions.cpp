2a456af82673dcd932e9f0b7be2b6f3ea1dc6db8
TheHonestGuy <92696767+Arif-Rahman-Sikder@users.noreply.github.com>
2021-12-15 23:18:32 +0600

    Create I. Welcome for you with Conditions.cpp
    This is the first problem that we have solved using the if-else statement. Here the code is programmed to print either "Yes" or "No" based on its input from the user.

---
+++ I. Welcome for you with Conditions.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+  int A, B;
+  cin >> A >> B;
+  if (A >= B) //if statement
+    {
+      cout << "Yes" << endl;
+    }
+  else //else statement
+  {
+    cout << "No" << endl;
+  }
+  return 0;
+}
